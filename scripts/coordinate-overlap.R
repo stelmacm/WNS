537e32a1df68ed4e4953940bf072e11b39e37c1f
Martin Stelmach <martinstelmach@hotmail.com>
2020-05-20 12:30:36 -0400

    Update coordinate-overlap.R

--- scripts/coordinate-overlap.R
+++ scripts/coordinate-overlap.R
@@ -1,50 +1,45 @@
-  all_results_clean <- filter(scraped, Type %in% c("Type.found_it", "Type.didnt_find_it", "Type.owner_maintenance", "Type.publish_listing"))
-  all_results_merge <- merge(all_results_clean, gc_filtered_dat, by = "GC")
+ all_results_merge <- scraped %>%
+    filter(Type %in% c("Type.found_it", "Type.didnt_find_it", "Type.owner_maintenance", "Type.publish_listing")) %>%
+    left_join(select(gc_filtered_dat,-X.1,-X,-lon,-lat)) %>%
+    mutate(county = paste0(trimws(gsub(pattern = "County.*$",replacement = "",county)),"-",province.state),
+    Year = lubridate::year(lubridate::ymd(Date)))
   
-  # lat.dms <- do.call(rbind, strsplit(as.character(all_results_merge$lat),":"))
-  # lat.dec <- as.numeric(lat.dms[, 1]) + (as.numeric(lat.dms[,2]) + as.numeric(lat.dms[, 3])/60)/60
-  # all_results_merge$lat <- lat.dec
-  # 
-  # lon.dms <- do.call(rbind, strsplit(as.character(all_results_merge$lon),":"))
-  # lon.dec <- as.numeric(lon.dms[, 1]) + (as.numeric(lon.dms[,2]) + as.numeric(lon.dms[, 3])/60)/60
-  # all_results_merge$lon <- lon.dec
-  
-  # char2dms(all_results_merge, chd = ":", chm = ":", chs = ":")
-  
-  all_results_merge$Year <- year(ymd(all_results_merge$Date))
+  # count users at each site per year  
+  # we do this again at a later step, but this is useful to tell if there are any sites that don't get visited in a year
   geocache.locs <- all_results_merge %>% 
-    # group_by(Year) %>% 
-    # mutate(count = length(unique(GC))) %>% 
-    group_by(GC, Year,lat.x,lon.x) %>% 
-    summarise(total = length(duplicated(User)))
+    group_by(GC, Year,lat,lon) %>%  
+    distinct(User) %>% 
+    tally(name = "total")
   
   # order of coords matters!
-  geocache.coords <- as_Spatial(st_as_sf(geocache.locs, coords = c("lat.x","lon.x"), crs = 4326, agr = "constant"))
+  geocache.coords <- as_Spatial(st_as_sf(geocache.locs, coords = c("lat","lon"), crs = 4326, agr = "constant"))
   
+  # create an index of matching polygons from presence data
   index.df <- as.data.frame(sp::over(geocache.coords, presence.poly, returnList = F))
   
-  colnames(index.df) <- "index"
-  results <- NULL
-  for (i in index.df$index) {
-    u <- presence.df[i, "STATEPROV"]
-    v <- presence.df[i, "COUNTYNAME"]
-    w <- presence.df[i, "SAMPLEDATE"]
-    x <- presence.df[i, "YR_SUSPECT"]
-    y <- presence.df[i, "YR_CONFIRM"]
-    z <- presence.df[i, "WNS_MAP_YR"]
-    results <- rbind.data.frame(results, cbind(u, v, w, x, y, z))
-  }
-  colnames(results) <- c("state.prov", "county", "sample.date", "yr.suspect", "yr.confirm", "wns.map.yr")
+  # bind results with coordinates of geocache sites (they are in the same order as the oringal index)
+  geocache.presence.df <- left_join(presence.df,all_results_merge,by=c("STATEPROV"="province.state","county"="county","year"="Year"))
   
-  geocache.presence.df <- cbind.data.frame(geocache.coords, results)
+  # merge with our scraped dataset of geocache records
+  # we want all, because there are a few sites that did not get visited some years
+  scraped$year<-lubridate::year(scraped$Date)
+  presence.scrape <-left_join(geocache.presence.df, scraped, by = c("GC", "year","User","Title","Log","lon","lat","Date","Type"))
+
+  # recode sites without visits to 0
+  is.na(presence.scrape$numfinds) <- 0
   
-  all_merge <- merge(geocache.presence.df, scraped, by = "GC",all=TRUE)
+  # fix date format
+  presence.scrape <- presence.scrape %>%
+    mutate(date = lubridate::ymd(date),
+    wns.map.yr = lubridate::ymd(gsub("-.+", "/01/01", WNS_MAP_YR)),
+    r.suspect = lubridate::ymd(gsub("-.+", "/01/01", YR_SUSPECT)),
+    yr.confirm = lubridate::ymd(gsub("-.+", "/01/01", YR_CONFIRM)))
   
-  all_merge$date <- ymd(all_merge$Date)
-  all_merge$wns.map.yr <- ymd(gsub("-.+", "/01/01", all_merge$wns.map.yr))
-  all_merge$yr.suspect <- ymd(gsub("-.+", "/01/01", all_merge$yr.suspect))
-  all_merge$yr.confirm <- ymd(gsub("-.+", "/01/01", all_merge$yr.confirm))
-  relevant.records <- all_merge %>% filter(wns.map.yr > Year)
-  relevant.records <- relevant.records[!is.na(relevant.records$GC),]
-  write.csv(relevant.records,"data/relevant-records.csv")
+  # we don't was to discard visits that would have preceeded introdution of infections!
+  # filter out geocache logs that were not made before WNS was "suspected"
+  write.csv(select(presence.scrape,-"geoms"),"data/relevant-records.csv")
+  
+  # relevant.records.wpoly<-left_join(relevant.records,presence.df,by=c("county"="COUNTYNAME"))
+  # saveRDS(relevant.records.wpoly,file = "data/relevant-records-withPoly.RDS")
+  
   list(time = Sys.time(), tempfile = tempfile())
