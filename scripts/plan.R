b527c1781479f8b9e1e3e30cd15168dc5b4174ec
Martin Stelmach <martinstelmach@hotmail.com>
2020-05-20 12:36:55 -0400

    Update plan.R

--- scripts/plan.R
+++ scripts/plan.R
@@ -4,15 +4,18 @@ plan <- drake_plan(
 
   ### Start with raw list
   # Read in GC list
-  gc_dat = read.csv("data/gc-list-unfiltered.csv",header=T,fill = T,sep = ",",na.strings = "",quote = "",comment.char = ""),
+  gc_dat = read.csv("data/gc-list-unfiltered.csv",header=T,row.names = NULL,fill = T,sep = ",",na.strings = "",quote = "",comment.char = ""),
+  
+  # generate url for scraping
+  gc_dat$url<-paste("https://www.geocaching.com/geocache/",gc_dat$GC,"_",gsub(" ","-",tolower(gsub("[^[:alnum:] ]", "", gc_dat$title))),sep=""),
   
   # Take a sample of the description sections from GC pages
   # source("scripts/sample_descriptions.R"),
   # sampled = sample.description(gc_dat),
   
   # clean the list of GC sites based on sampling keywords from descriptions
-  # source("scripts/filter_descriptions.R"),
-  # filtered = filter.description(gc_dat),
+  source("scripts/filter_descriptions.R"),
+  filtered = filter.description(gc_dat),
   gc_filtered_dat = na.omit(read.csv("data/gc-list-filtered.csv",header=T)),
   
   # Now scrape just the sites that have these keywords
@@ -24,20 +27,19 @@ plan <- drake_plan(
   # source("scripts/user_lookup.R"),
   # rev_lookup = rev_lookup(scrape_dat),
   
-  # Visualization of movement between sites
-  # source("scripts/geocache_mapping.R"),
-  # mapping = mapping(scrape_dat),
-  
-  # WNS presence at county level polygon
+    # WNS presence at county level polygon
   source("scripts/wns-presence.R"),
   
-
   # Find overlaps between GC sites and WNS infected counties
   source("scripts/coordinate-overlap.R"),
   
   # Matching GC sites to locations where we have genetic samples
   # source("scripts/coordinate-match.R"),
   # match = match_locations(scrape_dat,presence),
+  
+  # Visualization of movement between sites
+  source("scripts/geocache_mapping.R"),
+  mapping = mapping(scraped),
   
   # List of all counties within the States/Provinces in original GC list
   counties = read.csv("data/all-counties.csv", header = T) %>%
@@ -47,16 +49,16 @@ plan <- drake_plan(
   can.shape = readOGR("shape/lcd_000b16a_e/lcd_000b16a_e.shp"),
 
   # USA Counties shape file
-  usa.shape = map("county", regions = counties[counties$Country == "USA", ]$state.province, fill = T),
+  usa.shape = maps::map("county", regions = unique(counties[counties$Country == "USA", ]$state.province), fill = T),
 
   # Stich both countries together to create one polygon and fix names
   source("scripts/county-fix.R"),
   
   # Constructs spatial weight matricies from neighbour lists of county centroids
-  source("scripts/spatial-weight-matrix.R"),
+  # source("scripts/spatial-weight-matrix.R"),
 
-  # Constructs visit-based weights at the county level in long-format
-  source("scripts/geocache-weight.R")
+  # Constructs both adjacency and visit-based weights at the county level in long-format
+  source("scripts/geocache-weight.R"),
   
   # Not Run
   # source("scripts/mantel.R"),
@@ -77,8 +79,8 @@ plan <- drake_plan(
   # source("scripts/sim_runner.R"),
   # run_sim = run_sim(uc,wns_duration,county_duration),
   
-  # source("scripts/GLMmodelfitccavedata.R"),
-  # glm_model = glm_model(run_sim,overlap)
+  source("scripts/GLMmodelfitccavedata.R"),
+  glm_model = glm_model(run_sim,overlap)
   )
 
 good_config <- drake_config(plan)
