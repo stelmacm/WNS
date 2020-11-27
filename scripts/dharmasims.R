6c27378926a4942859b653a04763879752a16b4a
Ben Bolker <bbolker@gmail.com>
2020-11-27 11:31:54 -0500

    bmb notes

--- scripts/dharmasims.R
+++ scripts/dharmasims.R
@@ -6,6 +6,11 @@ library(ggmap)
 library(DHARMa)
 library(lme4)
 library(reshape)
+library(broom.mixed)
+library(GLMMadaptive) ## for higher AGQ
+library(purrr)
+library(dplyr)
+library(glmmTMB)
 usa <- map_data("usa")
 #Read in data
 modelresid <- read.csv("data/modelresiduals.csv")
@@ -74,8 +79,74 @@ modeldf <- read_csv("data/mixedmodeldf.csv")
 
 #I swear I had the glmer version working until last night???
 foimixedmodel2 <- glmer(formula, data = modeldf, family = binomial(link = "cloglog"), nAGQ = 0)
-#library(glmmTMB)
-#foimixedmodel2 <- glmmTMB(formula,data = modeldataframe, family = binomial(link = "cloglog")) #Back up option
+fm2_gT <- glmmTMB(formula,data = modeldf,
+                  family = binomial(link = "cloglog")) #Back up option
+
+vignette("mcmc", package="glmmTMB")
+vignette("model_evaluation",
+         package="glmmTMB")
+
+## higher nAGQ is always more accurate if it works/isn't infeasibly slow
+fm3 <- update(foimixedmodel2, nAGQ=1)
+fm2 <- foimixedmodel2
+tidy(foimixedmodel2)
+tidy(fm3)
+tidy(fm2_gT)
+mod_list<- list(nAGQ0=fm2,nAGQ1=fm3,glmmTMB=fm2_gT)
+tt <- (purrr::map_dfr(mod_list,tidy,.id="model")
+    %>% mutate(par=case_when(effect=="fixed" ~ "intercept",
+                             TRUE ~ group))
+)
+ggplot(tt,aes(estimate,par,colour=model)) +
+    geom_pointrange(aes(xmin=estimate-2*std.error,
+                        xmax=estimate+2*std.error),
+                    position=position_dodge(width=0.5))
+## standard errors are extremely different... why?
+
+## we can't do nAGQ>1 for this problem in lme4 ...
+
+## I don't think mixed_model will do multiple (crossed) RE?
+m1 <- mixed_model(incidence ~ offset(log(previousyear +1)),
+            ## random = ~ 1|year, ## + (1|county),
+            random = ~ 1 | year, ##  + 1 | county,
+            family=binomial(link="cloglog"),
+            data=modeldf)
+
+## INLA: integrated nested Laplace approximation
+##  off-CRAN package
+##  broom.mixed?
+
+## comparing nAGQ=0 to nAGQ=1
+p2 <-predict(foimixedmodel2)
+p3 <- predict(fm3)
+
+plot(p2,p2-p3, xlab="prediction from model 2 (cloglog scale)",
+     ylab="pred diff")
+## predictions vary about +/- 0.2 log-hazard, and most at the extremes
+
+## fit model with FOI (or whatever ...) as a predictor as well as an offset
+fm4 <- update(fm2, nAGQ=1,
+              . ~ . + log(previousyear+1))
+
+## check all the optimizers ...
+## ?lme4::convergence
+af <- allFit(fm3)
+
+## ??
+with(modeldf, table(incidence, previousyear))
+## take out 1 -> 0 transitions
+tmpdf <- filter(modeldf, !(incidence==0 & previousyear==1))
+bogus1 <- glm(incidence ~log(previousyear+1),
+    data=tmpdf,
+    family=binomial(link="cloglog"))
+summary(bogus1)
+
+bogus2 <- glm(incidence ~log(previousyear+1),
+              data=tmpdf,
+              family=binomial())
+summary(bogus2)
+
+
 #Doing DHARMa sim from the model bc thats how DHARMa works
 simoutput <- simulateResiduals(fittedModel = foimixedmodel2)
 plot(simoutput)
@@ -95,3 +166,4 @@ simoutput2 <- recalculateResiduals(simoutput, group = modeldf$year)
 plot(simoutput2) #Ew
 
 #My next thoughts are creating number of observations at a given county and including that into the mixed model
+
