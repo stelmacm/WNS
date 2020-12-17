c5e6f87a8eab154b5383277e2974f3ee4983a616
Martin Stelmach <martinstelmach@hotmail.com>
2020-12-17 00:52:05 -0500

    Update howdoidothings.R

--- scripts/howdoidothings.R
+++ scripts/howdoidothings.R
@@ -1,144 +1,147 @@
-#The real DHARMa SIMS code block
-library(tidyverse)
-library(lme4)
-library(DHARMa)
-library(glmmTMB)
-library(brms)
-library(glmmADMB)
-library(lattice)
-library(broom.mixed)
-library(viridisLite)
+#Create azzalini power exp and test with different parameters abd compare MLE's
+source("scripts/packages.R")
 
-mixedmodeldf <- read.csv("data/mixedmodeldf.csv")
-mixedmodeldf$year <- factor(mixedmodeldf$year)
-mixedmodeldf$previnf <- (lag(mixedmodeldf$incidence))
+#Import dataset
+mixedmodeldf <- read.csv("data/incidencepercounty.csv") %>%
+  as_tibble() %>%
+  mutate(year = factor(year))
 
-# So problem with mixed modeldf is that the years continue once incidence has 
-# occurred. So it is redundant.
 
-#Changing to disappear after incidence occurs
-newdf <- mixedmodeldf %>% filter((incidence == 0) | (incidence == 1 & previnf == 0))
+#Testing different types of coord to km types
+source("scripts/wns-presence.R")
+
+presence.scrape <- read.csv("data/relevant-records.csv")
+
+#Remove Cali and Wash for now
+uniq.df <- (presence.df
+            %>% dplyr::filter(!STATEPROV %in% c("California","Washington"),
+                              !duplicated(county)) #so we only have unique counties
+)
+
+wnslat <- map_dbl(uniq.df$geoms, ~st_centroid(.x)[[1]])
+wnslon <- map_dbl(uniq.df$geoms, ~st_centroid(.x)[[2]])
+
+wns.center.coords <- cbind(wnslat, wnslon)
+
+d1<- distm(wns.center.coords, fun = distGeo)
+dimnames(d1) <- list(uniq.df$county,uniq.df$county)
+
+diag(d1) <- 0 
+#convert from m to km
+d1 <- d1/1000
+mledf <- data.frame(NULL)
+
+#Azzalini loop function 
+for (param in seq(2,100,1)){
+  
+  
+ mixedmodeldf <- read.csv("data/incidencepercounty.csv") %>%
+    as_tibble() %>%
+    mutate(year = factor(year))
+#Remove things that don't work for me later on
+county.incidencee.by.year <- mixedmodeldf[-c(27,28,29,30,31,32,33,
+                                             34,35,36,37,38,39,
+                                             5201, 5202,5203,5204,
+                                             5205, 5206,5207,5208,5209,
+                                             5210,5211,5212,5213,
+                                             7151,7152,7153,7154,7155,7156
+                                             ,7157,7158,7159,7160,7161,7162,7163),-4]
+#Only way I could figure it out is via
+countylist <- county.incidencee.by.year %>%
+  arrange(year) %>%
+  filter(year == 2006) %>%
+  dplyr::select(county)
+
+#Maybe just have to do it by hand
+azzelinifun <- exp(-((d1)/param)^2)
+diag(azzelinifun) <- 0
+azzelinifun[(azzelinifun < 1e-100)] <- 0
+
+#Now to weight matrix
+localcountymat.w <- mat2listw(azzelinifun, style = "W")
+localcountymat.m <- as(localcountymat.w, "CsparseMatrix") 
+localcountymat <- as.matrix(localcountymat.m)
+
+
+#Create forloop to create dataframe
+#Create dataframe of county by year filled with incidence
+#Retake first year english so that sentence makes sense
+for(i in levels(mixedmodeldf$year)){
+  countylist[,i] <- county.incidencee.by.year %>%
+    arrange(year) %>%
+    filter(year == i) %>%
+    dplyr::select(incidence)
+}
+#view(countylist)
+
+
+countylist <- as.data.frame(countylist[,-1])
+
+#Now we need to multiply W_ij by every I_t 
+#Another for loop
+
+#Create a base layer to begin with
+foidf <- county.incidencee.by.year %>%
+  arrange(year) %>%
+  filter(year == 2006) %>%
+  dplyr::select(county)
+
+
+#For loop that creates force of infection datafrome
+for (i in levels(mixedmodeldf$year)) {
+  #need to detach
+  infectionvector <- countylist[,i]
+  infectionvector <- as.matrix(infectionvector)
+  #multiply W_ij %*% I_t
+  foivector <- localcountymat %*% infectionvector
+  #reattach
+  foidf[,i] <- foivector
+}
+
+
+#The melt looks something like this
+forceofinfectiondf <- reshape2::melt(foidf, id = "county") %>%
+  dplyr::rename(year = 'variable', foi = 'value') %>%
+  arrange(county) %>%
+  mutate(year = factor(year))
+#Never arranged alphabetically before. odd 
+
+incidencedata <- (read.csv("data/incidencepercounty.csv"))%>%
+  dplyr::select(county, year, incidence) %>%
+  mutate(year = factor(year))
+
+modeldataframe <- left_join(forceofinfectiondf, incidencedata, by = c("county","year")) %>%
+  mutate(previousyear=lag(incidence)) %>%
+  filter(year != "2006") #Maybe there are nicer ways to do this. oh well
+
+modeldataframe$year <- factor(modeldataframe$year)
+modeldataframe$previnf <- (lag(modeldataframe$foi))
+
+#So problem with mixed modeldf is that the years continue once incidence has 
+#occered. So it is redundant.
+
+#Changing to disappear after incidence occures
+newdf <- modeldataframe %>% subset((incidence == 0) | (incidence == 1 & previousyear == 0))
 
 #just glm model
-foimodel <- glm(incidence ~ offset(log(previousyear + 1)),
+foimodel <- glm(incidence ~ offset(log(previnf + 1)),
                 data = newdf, family = binomial(link = "cloglog"))
-summary(foimodel)
-op <- par(mfrow=c(2,2)); plot(foimodel); par(op)
-#So glm looks good.
-glmsims <- simulateResiduals(foimodel)
-plot(glmsims) #I dont think I underestand res vs pred
 
-with(newdf,hist(log(previousyear+1)))
-with(newdf,plot(table(log(previousyear+1))))
-with(newdf, table(previousyear==0,incidence))
-## ?? why do half the values in the data set have FOI=0?
-## this is OK for now
+mledf[param,1]<- logLik(foimodel)
+}
 
-plotResiduals(foimodel, newdf$foi) #this makes sense I think
+x <- seq(1,200,1)
+plot(x,mledf$V1)
 
-formula <- incidence ~ (1|year) + (1|county) + offset(log(previousyear + 1))
-
-foimm <- glmer(formula, data = newdf, family = binomial(link = "cloglog"))
-af <- allFit(foimm)  ## try everything
-ss <- summary(af)
-nll <- -1*ss$llik
-nlls <- nll - min(nll)
-cc <- cbind(ss$fixef,ss$sdcor,nlls)
-pairs(cc,cex=2,gap=0)
-
-summary(foimm) #Year is a ~tad~ high
-
-glmmsims <- simulateResiduals(foimm)
-plot(glmmsims) #Not that nice...
-
-plotResiduals(foimm)
-
-foimm3 <- update(foimm, control=glmerControl(optimizer="Nelder_Mead"))
-## could look at predictions etc etc with this fit and see that they're
-## really not all that different ...
-                 
-#Trying to cheat...Maybe wrong approach to large eigenvalue problem
-foimm2 <- glmer(formula, data = newdf, family = binomial(link = "cloglog"),
-                control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5)))
-
-#Trying glmmTMB
-foiTMB <- glmmTMB(formula, data = newdf, family = binomial(link = "cloglog"))
-summary(foiTMB) #Its actually a bit worse?
-#Still cant figure it out...
-tmbsims <- simulateResiduals(foiTMB) #oof
-plot(tmbsims)
-
-rr <- ranef(foiTMB)$cond
-class(rr) <- "ranef.mer"
-
-dotplot(rr)$year #Good
-dotplot(rr)$county #Oh my....
-aa <- as.data.frame(rr)
-
-## could take this data frame and join it with county-level
-## info on 'first year infected'
-first_inf <- (newdf
-    %>% filter(incidence==1)
-    %>% group_by(county)
-    %>% arrange(year)
-    %>% slice_min(1)
-    %>% dplyr::select(county,year)
-    %>% rename(first_yr=year)
-)
-
-aa2 <- (aa
-    %>% as_tibble()
-    %>% filter(grpvar=="county")
-    %>% rename(county=grp)
-    %>% dplyr::select(county,condval,condsd)
-    %>% full_join(first_inf,by="county")
-    %>% mutate(county=reorder(factor(county),condval))
-)
-
-gg0 <- ggplot(aa, aes(x=condval, xmin=condval-2*condsd, xmax=condval+2*condsd,
-               y=grp)) +
-    geom_pointrange()
-
-gg0 + facet_wrap(~grpvar,ncol=2,scale="free")
-
-ggplot(aa2, aes(x=condval, xmin=condval-2*condsd, xmax=condval+2*condsd,
-               y=county, colour=first_yr)) +
-    geom_pointrange() +
-    scale_colour_viridis_d()
+loglikdf <- cbind(x,mledf$V1)
+colnames(loglikdf) <- c("X","Y")
+loglikdf <- as.data.frame(loglikdf)
+p <- ggplot(loglikdf, aes(X,Y,group = 1)) +
+  geom_line()+
+  geom_point() 
+  
+ggsave("figures/loglikelihoodofazzalini.png", plot = p)
 
 
 
-#going to lightly try going bayesian
-foibrms <- brm(formula, data = newdf, family = binomial(link = "cloglog"))
-summary(foibrms) #Terrible, Uglt
-
-foibrmpairs <- pairs(foibrms) #See WNS/figures/brmspairs.png
-
-## only two things that might be worth playing with on the GLMM end:
-## * add a linear trend of year (as below)
-## * see what happens if FOI is a predictor rather than an offset
-
-## Y = exp(O)*exp(a+bx) = exp(O+a*bx)
-## this assumes that Y is _strictly proportional_ to exp(O)
-## what if we allow O to have a coefficient?
-##  Y = exp(a+b*x+c*O)
-## if O != 1 is a better fit ...
-## Y = exp(c*O)*exp(a+b*x) = exp(O)^c*exp(a+b*x)
-## so it means that whatever our offset (FOI, area, time observed, effort, ...)
-##  the outcome is a POWER LAW of that offset rather than strictly proportional
-##  e.g. diminishing returns
-## if you say Y ~ offset(O) + O + X
-##  it says Y=exp(O)*exp(c*O)*exp(a+b*X)
-## the null value is proportionality, c measures difference from proportional
-##  (c==0 we get back to the old offset model, c>0 accelerating, c<0 ..)
-## I DON'T KNOW WHAT hazard not strictly proportional to FOI means in an epidemiological model!
-##  FOI = beta*I
-##  (beta*I)*S
-##  (beta*I)^c * S  ... ???
-
-
-#Questions:
-#I think it is unintuitive to me how to play around with formula
-#ie mixedmodeldf$c_year <- as.numeric(as.character(mixedmodeldf$year))-2007
-#formula <- incidence ~ 1 + c_year + (1|year) + (1|county) + offset(log(previousyear + 1))
-#Unsure if this 
+logLik(foimodel)
