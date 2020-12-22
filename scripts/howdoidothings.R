0dc93380cc08329da21f7ce530f02c3fcda8e990
Martin Stelmach <martinstelmach@hotmail.com>
2020-12-22 12:30:44 -0500

    Update howdoidothings.R

--- scripts/howdoidothings.R
+++ scripts/howdoidothings.R
@@ -1,147 +1,57 @@
-#Create azzalini power exp and test with different parameters abd compare MLE's
-source("scripts/packages.R")
+library(tidyverse)
+library(DHARMa)
+library(sf)
+library(sp)
+library(spdep)
 
-#Import dataset
-mixedmodeldf <- read.csv("data/incidencepercounty.csv") %>%
-  as_tibble() %>%
-  mutate(year = factor(year))
+bestloglik <- read_csv("data/azzaliniparam.csv") %>%
+  mutate(theta = factor(theta))
 
+zoomedinalittle <- bestloglik %>% filter(ID > 5 & ID < 150)
 
-#Testing different types of coord to km types
-source("scripts/wns-presence.R")
+ggplot(data = zoomedinalittle, aes(x = ID, y = Loglikelihood)) +
+  geom_line(aes(color = theta))
 
-presence.scrape <- read.csv("data/relevant-records.csv")
-
-#Remove Cali and Wash for now
-uniq.df <- (presence.df
-            %>% dplyr::filter(!STATEPROV %in% c("California","Washington"),
-                              !duplicated(county)) #so we only have unique counties
-)
-
-wnslat <- map_dbl(uniq.df$geoms, ~st_centroid(.x)[[1]])
-wnslon <- map_dbl(uniq.df$geoms, ~st_centroid(.x)[[2]])
-
-wns.center.coords <- cbind(wnslat, wnslon)
-
-d1<- distm(wns.center.coords, fun = distGeo)
-dimnames(d1) <- list(uniq.df$county,uniq.df$county)
-
-diag(d1) <- 0 
-#convert from m to km
-d1 <- d1/1000
-mledf <- data.frame(NULL)
-
-#Azzalini loop function 
-for (param in seq(2,100,1)){
-  
-  
- mixedmodeldf <- read.csv("data/incidencepercounty.csv") %>%
-    as_tibble() %>%
-    mutate(year = factor(year))
-#Remove things that don't work for me later on
-county.incidencee.by.year <- mixedmodeldf[-c(27,28,29,30,31,32,33,
-                                             34,35,36,37,38,39,
-                                             5201, 5202,5203,5204,
-                                             5205, 5206,5207,5208,5209,
-                                             5210,5211,5212,5213,
-                                             7151,7152,7153,7154,7155,7156
-                                             ,7157,7158,7159,7160,7161,7162,7163),-4]
-#Only way I could figure it out is via
-countylist <- county.incidencee.by.year %>%
-  arrange(year) %>%
-  filter(year == 2006) %>%
-  dplyr::select(county)
-
-#Maybe just have to do it by hand
-azzelinifun <- exp(-((d1)/param)^2)
-diag(azzelinifun) <- 0
-azzelinifun[(azzelinifun < 1e-100)] <- 0
-
-#Now to weight matrix
-localcountymat.w <- mat2listw(azzelinifun, style = "W")
-localcountymat.m <- as(localcountymat.w, "CsparseMatrix") 
-localcountymat <- as.matrix(localcountymat.m)
+#I dont think this one makes sense but that is what has the best MLE
+#Unsure basically which one to select if theyre all so close
+#The thing is that all of these are not very "sparse" and the matrix is very dense
 
 
-#Create forloop to create dataframe
-#Create dataframe of county by year filled with incidence
-#Retake first year english so that sentence makes sense
-for(i in levels(mixedmodeldf$year)){
-  countylist[,i] <- county.incidencee.by.year %>%
-    arrange(year) %>%
-    filter(year == i) %>%
-    dplyr::select(incidence)
-}
-#view(countylist)
+optimalone <- read_csv("data/mixedmodelpoint933.csv") %>%
+  mutate(year = factor(year)) %>%
+  mutate(incidence = factor(incidence))
 
+formula <- incidence ~ (1|year) + (1|county) + offset(log(previnf + 1))
 
-countylist <- as.data.frame(countylist[,-1])
+foimm <- glmer(formula, data = optimalone, family = binomial(link = "cloglog"))
+summary(foimm)
 
-#Now we need to multiply W_ij by every I_t 
-#Another for loop
+#Doing DHARMa sim from the model bc thats how DHARMa works
+simoutput <- simulateResiduals(fittedModel = foimm)
+plot(simoutput)
+hist(simoutput)
+#Now at 1.4 and 70 and compare models
 
-#Create a base layer to begin with
-foidf <- county.incidencee.by.year %>%
-  arrange(year) %>%
-  filter(year == 2006) %>%
-  dplyr::select(county)
+optimalone2 <- read_csv("data/mixedmodel1point470.csv") %>%
+  mutate(year = factor(year)) %>%
+  mutate(incidence = factor(incidence))
 
+foimm2 <- glmer(formula, data = optimalone2, family = binomial(link = "cloglog"))
+summary(foimm2)
 
-#For loop that creates force of infection datafrome
-for (i in levels(mixedmodeldf$year)) {
-  #need to detach
-  infectionvector <- countylist[,i]
-  infectionvector <- as.matrix(infectionvector)
-  #multiply W_ij %*% I_t
-  foivector <- localcountymat %*% infectionvector
-  #reattach
-  foidf[,i] <- foivector
-}
+#Doing DHARMa sim from the model bc thats how DHARMa works
+simoutput2 <- simulateResiduals(fittedModel = foimm2)
+plot(simoutput2)
+hist(simoutput2)
 
+optimalone3 <- read_csv("data/mixedmodel2and96.csv") %>%
+  mutate(year = factor(year)) %>%
+  mutate(incidence = factor(incidence))
 
-#The melt looks something like this
-forceofinfectiondf <- reshape2::melt(foidf, id = "county") %>%
-  dplyr::rename(year = 'variable', foi = 'value') %>%
-  arrange(county) %>%
-  mutate(year = factor(year))
-#Never arranged alphabetically before. odd 
+foimm3 <- glmer(formula, data = optimalone3, family = binomial(link = "cloglog"))
+summary(foimm3)
 
-incidencedata <- (read.csv("data/incidencepercounty.csv"))%>%
-  dplyr::select(county, year, incidence) %>%
-  mutate(year = factor(year))
-
-modeldataframe <- left_join(forceofinfectiondf, incidencedata, by = c("county","year")) %>%
-  mutate(previousyear=lag(incidence)) %>%
-  filter(year != "2006") #Maybe there are nicer ways to do this. oh well
-
-modeldataframe$year <- factor(modeldataframe$year)
-modeldataframe$previnf <- (lag(modeldataframe$foi))
-
-#So problem with mixed modeldf is that the years continue once incidence has 
-#occered. So it is redundant.
-
-#Changing to disappear after incidence occures
-newdf <- modeldataframe %>% subset((incidence == 0) | (incidence == 1 & previousyear == 0))
-
-#just glm model
-foimodel <- glm(incidence ~ offset(log(previnf + 1)),
-                data = newdf, family = binomial(link = "cloglog"))
-
-mledf[param,1]<- logLik(foimodel)
-}
-
-x <- seq(1,200,1)
-plot(x,mledf$V1)
-
-loglikdf <- cbind(x,mledf$V1)
-colnames(loglikdf) <- c("X","Y")
-loglikdf <- as.data.frame(loglikdf)
-p <- ggplot(loglikdf, aes(X,Y,group = 1)) +
-  geom_line()+
-  geom_point() 
-  
-ggsave("figures/loglikelihoodofazzalini.png", plot = p)
-
-
-
-logLik(foimodel)
+#Doing DHARMa sim from the model bc thats how DHARMa works
+simoutput3 <- simulateResiduals(fittedModel = foimm3)
+plot(simoutput3)
+hist(simoutput3)
