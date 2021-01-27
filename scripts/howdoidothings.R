6c532000b38649cd27f100bf4cf8535f5f46af6d
Martin Stelmach <martinstelmach@hotmail.com>
2021-01-27 09:41:44 -0500

    Update howdoidothings.R

--- scripts/howdoidothings.R
+++ scripts/howdoidothings.R
@@ -1,51 +1,69 @@
-What is the point of the : after declaring the arguements of the function? 
+#January 27th Meeting notes
 
-template <typename T>
-matrix<T>::matrix(const size_t &input_rows, const size_t &input_cols, const initializer_list<T> &input_elements)
-    : matrix(input_rows, input_cols, vector<T>{input_elements}) {}
+library(tidyverse)
 
-or
+#The data frame with the best parameters
+hessian <- read.csv("data/paramhessian.csv")
+hessian <- hessian[,-1]
+hessian.inv <- solve(hessian)
+param.se <- sqrt(diag(hessian.inv))
+param.se
 
-template <typename T>
-matrix<T>::matrix(const matrix<T> &m)
-    : rows(m.rows), cols(m.cols)
-{
-    elements = new T[rows * cols];
-    for (size_t i{0}; i < rows * cols; i++)
-        elements[i] = m.elements[i];
+#Create Confidence interval matrix 
+CI.matrix <- as.data.frame(matrix(NA, nrow = 3, ncol = 3))
+#Parameters from optim function 
+bestparams <- c(9.501744, -1.066294, -6.065743)
+
+CI.matrix[1,] <- bestparams
+CI.matrix[2,] <- bestparams - 1.96 * param.se
+CI.matrix[3,] <- bestparams + 1.96 * param.se
+names(CI.matrix) <- c("scaling parameter", "theta", "a")
+rownames(CI.matrix) <- c("ML", "95% Lower bound", "95% Upper bound")
+
+CI.matrix$theta <- plogis(CI.matrix$theta)*2
+CI.matrix$a <- exp(CI.matrix$a)
+
+CI.matrix
+#And I hope this is the CI of each param
+
+#What I actually wanted to do
+
+newdf <- read.csv("data/bestparammixeddf.csv")
+library(bbmle)
+#The construction of thinge needed to run the function
+source("scripts/packages.R")
+source("scripts/countylistanddistmatrix.R") 
+source("scripts/parametrizefunction.R") #Just the function
+
+#Now to use mle2 wrapper of optim instead of optim I need to choose appropriate
+#Values from the mixed model df which is what newdf
+
+#when using bbmle I need to know what I have
+#p = 0.50
+#N = 546 (Number of unique counties)
+#k = 12 (number of years to infect a county??)
+#But once a county is infected, the data for the remaining years is cut
+
+#Now I want to use newdf to create the mle2 thing
+subsetdf <- newdf %>% filter(incidence == 1)
+
+#Tried lots of things and this was the one that made me feel I
+#was going in the right direction
+
+#p = prob, k = success, N= number of trials 
+binomNLL1 = function(p, k, N) {
+  -sum(dbinom(k, prob = p, size = N, log = TRUE)) 
 }
+unique(newdf$county)
+nrow(subsetdf)
+#I realize this looks like a bad attempt but there were 100 others that were much worse
 
-How can this be applied to classes/ Did I apply this to classes correctly?
+attempt3 <- mle2(minuslogl = binomNLL1, start = list(p = .5),
+                 data = list(N = nrow(546), k = 12))
 
-class solve_OLS
-{
-public:
-    solve_OLS(matrix<double> &X, matrix<double> &Y)
-        : R(X.get_rows(), Y.get_cols())
-    {
-    }
-    matrix<double> solve();
 
-    class x_and_y_not_same_size
-    {
-    };
+#Also realized that with the current parameters, creating the weight matrix 
+#all the counties are connected
 
-    class matrix_is_empty
-    {
-    };
-
-private:
-    matrix<double> R;
-};
-
-matrix<double> solve_OLS::solve()
-{
-    R(1, 1) = 0;
-    return R;
-}
-
-I want to show that I create my own template as well. Should I do this for this class type?
-
-Easy part will be implementing shooting algorithm/working with the actual lasso regression part.
-Difficulty is with making it smooth and seamless and have proper classes and member functions that work together.
-Consideration for releading memory at the end of the function to only keep what is necessary.
+source("scripts/fullmodeltorun.R")
+Matrix::image(localcountymat.m)
