c806d57434b950166132212a5c0cdaa070e27bd7
Martin Stelmach <martinstelmach@hotmail.com>
2021-02-17 10:02:44 -0500

    Update howdoidothings.R

--- scripts/howdoidothings.R
+++ scripts/howdoidothings.R
@@ -1,47 +1,91 @@
-#For BMB meeting Feb 10 2020
+source("scripts/packages.R")
+source("scripts/wns-presence.R")
 
-#Last we left off:
-#Tried to recreate shared users from code from Adrian
-#Results looked like a crime scene
-#BMB suggested to look through even earlier stuff than the shared users df
-#Went right to the scrape and went from therer
+shareuserlists <- read.csv("data/fullshareduserslist.csv") %>%
+  rename(county = county1) %>%
+  rename(year = i.year) %>%
+  mutate(year = factor(year)) %>%
+  filter(year != 2001) %>%
+  filter(year != 2002) %>%
+  filter(year != 2003) %>%
+  filter(year != 2004) %>%
+  filter(year != 2005) %>%
+  filter(year != 2006)
 
-#Problems that may not be problems:
-#Even though my WNS repo is a "clone" (not gitclone more like stormtrooper type of clone)
-#The csv files are different enough that they are causing different results.
-#I took the one with more data (the original) and have them both in here
+#Going to build other matrix  
+uniq.df <- (presence.df
+            %>% dplyr::filter(!STATEPROV %in% c("California","Washington"),
+                              !duplicated(county)) #so we only have unique counties
+)
 
-#Solutions:
-#Went right to scrape and re-organized all user information in order to create shared user
-#Data frame has lots of 3822 users visiting more than 1 county throughout the time in question
+wnslat <- map_dbl(uniq.df$geoms, ~st_centroid(.x)[[1]])
+wnslon <- map_dbl(uniq.df$geoms, ~st_centroid(.x)[[2]])
 
-#Problems that are actually problems:
-#Went through and made counties from GC coords and identified counties.
-#Unfortunately Canada does not have any resources for counties (since there are so many types)
-#and so many of them (only 33 once I leftjoined all info about CAD counties I could find) had
-#to be implented by hand. Should I do something better than this? 
-#It was done for USA counties but just can't find CAD data for county level
+wns.center.coords <- cbind(wnslat, wnslon)
 
-#Another problem was a gsub for strings of counties. I realized it does not work with 2 name
-#states ie New York, New Jersey ect. 
+d1 <- distm(wns.center.coords, fun = distGeo)
+dimnames(d1) <- list(uniq.df$county,uniq.df$county)
 
-#Making full model is easy after this step. 
+diag(d1) <- 0 
+#convert from m to km
+d1 <- d1/1000
+bothcounties <- melt(d1)
+bothcounties <- bothcounties %>% rename(county = Var1) %>% rename(county2 = Var2) 
 
-#After that it will be scaling the shared users matrix and maybe possibly addressing JD's 
-#point about having it as a rixed effect? 
+#Take known data
+mixedmodeldf <- read.csv("data/incidencepercounty.csv") %>%
+  as_tibble() %>%
+  mutate(year = factor(year))
 
-#Code I think we might need to look at
-source("scripts/packages.R") #import just in case
-#The difference between MS and AF data on shared users
-source("scripts/huntingforsharedusers.R")
-source("scripts/huntingfordifferentsharedusers.R")
-source("scripts/lonlattocountyconversion.R")
-#Finally creation of shared users df
-source("scripts/creatingsharedusersdf.R")
+county.incidence.by.year <- mixedmodeldf %>% filter(county != "Lewis-Washington" &
+                                                      county != "King-Washington" &
+                                                      county != "Plumas-California") %>%
+  dplyr::select(-yc)
 
+countylist <- county.incidence.by.year %>%
+  arrange(year) %>%
+  filter(year == 2006) %>%
+  dplyr::select(county)
 
-##FIT:
-##  W_S = spatial weight matrix (scale, exponent)
-##  W_C = caver weight matrix (shared visits)
-## offset = log(W_S %*% incidence  + (W_C %*% incidence)/scale?? + min)
-##   (alpha*space + (1-alpha)*cavers)
+azzelinifun <- exp(-((d1)/9.508066)^0.512292358) #Use old vals for now
+diag(azzelinifun) <- 0
+
+#Now to weight matrix
+localcountymat.w <- mat2listw(azzelinifun, style = "W")
+localcountymat.m <- as(localcountymat.w, "CsparseMatrix") 
+localcountymat <- as.matrix(localcountymat.m)
+
+#Create forloop to create dataframe
+#Create dataframe of county by year filled with incidence
+#Retake first year english so that sentence makes sense
+for(i in levels(mixedmodeldf$year)){
+  countylist[,i] <- county.incidence.by.year %>%
+    arrange(year) %>%
+    filter(year == i) %>%
+    dplyr::select(incidence)
+}
+#view(countylist)
+
+countylist <- as.data.frame(countylist[,-1])
+
+#Now we need to multiply W_ij by every I_t 
+#Another for loop
+
+#Create a base layer to begin with
+foidf <- county.incidence.by.year %>%
+  arrange(year) %>%
+  filter(year == 2006) %>%
+  dplyr::select(county)
+
+#Can't order for some reason??
+
+#For loop that creates force of infection datafrome
+for (i in levels(mixedmodeldf$year)) {
+  #need to detach
+  infectionvector <- countylist[,i]
+  infectionvector <- as.matrix(infectionvector)
+  #multiply W_ij %*% I_t
+  foivector <- localcountymat %*% infectionvector
+  #reattach
+  foidf[,i] <- foivector
+}
