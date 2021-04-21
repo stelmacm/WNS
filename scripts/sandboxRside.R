5b54977d48f86d16f06dd3671f9e23b9a51f7093
Ben Bolker <bbolker@gmail.com>
2021-04-21 11:40:46 -0400

    one more note

--- scripts/sandboxRside.R
+++ scripts/sandboxRside.R
@@ -15,8 +15,18 @@ compile("scripts/sandbox.cpp")
 dyn.load(dynlib("scripts/sandbox"))
 obj <- MakeADFun(data = data, parameters=parameters, DLL="sandbox")
 
+## create a list of 
 lapply(
     yearvec,
     function(y) {
         sparse.model.matrix(~county-1, data=subset(all_data,year==y))
     })
+## then within TMB:
+## extract Z[i] from the list
+Z * county_effect
+## if you have one obs per county per year, always in the same order,
+## county order == obs order, then Z is diagonal
+## or you could say
+loghazard[i] += county_effect[i]
+## or
+loghazard[j] += county_effect[county[j]]
