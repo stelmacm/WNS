be278b4764563617dd97b27f1a4f06f3528bd9cc
Martin Stelmach <martinstelmach@hotmail.com>
2021-05-19 10:03:36 -0400

    Update howdoidothings.R

--- scripts/howdoidothings.R
+++ scripts/howdoidothings.R
@@ -1,64 +1,30 @@
 source("scripts/creatingfunctiondf.R")
 library(TMB)
-compile("scripts/stepbystepTMBmodel.cpp")
-dyn.load(dynlib("scripts/stepbystepTMBmodel"))
-countyeffect = rep(0,548)
-yeareffect = rep(0,13)
-dparam = 50
-thetaparam = 1.4
-rhoparam = 0.6  
+#I am curious about priors
+#How do I make shape of dist matrix have to do with anything
+distmat <- read.csv("data/distancematrix.csv")
+#looking at a melted distmat
+meltdist <- melt(distmat)
+#It will account for distances twice but who cares
+hist((meltdist$value)) #might be useful when thinking of priors
 
-a <- 0.001
-dd <- list(dist = orderedmat, dim = 548, SM = bigsharedusers, numberofyears = 13,
-           fullcountyincidence = countylist)
-pp <- list(log_d = log(dparam), theta = thetaparam, logit_rho = qlogis(rhoparam),  log_offsetparam = log(a),
-           logsd_County = 0, logsd_Year = 0,
-           YearRandomEffect = yeareffect, CountyRandomEffect = countyeffect)
-
-obj <- MakeADFun(data=dd, parameters=pp, DLL="stepbystepTMBmodel",
-                 silent=TRUE,
-                 random=c("YearRandomEffect","CountyRandomEffect"))
-
-
-opt2 <- with(obj, nlminb(start = par, obj = fn, gr=gr,
-                         control=list(trace=10)))
-#LL of 967.19
-
-#Post Monday Meeting  with BMB
-#Runnning his adjustments to see if they get the same results
-
-compile("scripts/BMBTMB.cpp")
-dyn.load(dynlib("scripts/BMBTMB"))
-
-
-
-obj2 <- MakeADFun(data=dd, parameters=pp, DLL="BMBTMB",
-                 silent=TRUE,
-                 random=c("YearRandomEffect","CountyRandomEffect"))
-
-optBMB <- with(obj2, nlminb(start = par, obj = fn, gr=gr,
-                         control=list(trace=10)))
-
-opt2BMB <- optim(par = optBMB$par, fn = obj2$fn, gr = obj2$gr, method = "BFGS", control=list(trace=TRUE))
-#Oh.... ??? LL = 978.865
-
-opt3BMB <- with(obj2, nlminb(start = opt2BMB$par, obj = fn, gr=gr,
-                             control=list(trace=10)))
-
-optBMB$par 
-##LL of 1219.97
-#log_d           theta            logit_rho    log_offsetparam    logsd_Year    logsd_County
-#-11.2104551       0.2622527      -1.2418091      -3.8789451      -1.0126715  -354.8913564 
-
-obj2$fn(optBMB$par)
-#Works but seems vastly different from the original step by step TMB
-
-
+#When I adjust my priors and run stuff 
 compile("scripts/stepbystepwithpriors.cpp")
 dyn.load(dynlib("scripts/stepbystepwithpriors"))
 
+distmean <- function(lower, upper){
+  distmiddle <- mean((lower:upper))
+  distsd <- (upper - lower)/6
+  return(c(distmiddle, distsd))
+}
+
+distpriors <- distmean(10, 1500)
+#It does not accept the values from the function into the list? Get NaN for obj3$fn(obj3$par)
 dd1 <- list(dist = orderedmat, dim = 548, SM = bigsharedusers, numberofyears = 13,
-            fullcountyincidence = countylist, dmean = 80, dsd = 23.33, thetamean = 1.5, thetasd = 0.4166, offsetmean = 0.5, offsetsd = 0.15)
+            fullcountyincidence = countylist, dmean = log(800), dsd = log(230.333), thetamean = 1.25, 
+            thetasd = .4583)#, offsetmean = 0.5, offsetsd = 0.15)
+#Hard coded offset priors bc logitnormal looks like something I dont want to play with too much
+
 pp1 <- list(log_d = log(dparam), theta = thetaparam, logit_rho = qlogis(rhoparam),  log_offsetparam = log(a),
             logsd_County = 0, logsd_Year = 0,
             YearRandomEffect = yeareffect, CountyRandomEffect = countyeffect)
@@ -68,26 +34,25 @@ obj3 <- MakeADFun(data=dd1, parameters=pp1, DLL="stepbystepwithpriors",
                   random=c("YearRandomEffect","CountyRandomEffect"))
 
 optprior <- with(obj3, nlminb(start = par, obj = fn, gr=gr,
-                            control=list(trace=10)))
+                              control=list(trace=10)))
 
- opt2BMB <- optim(par = optBMB$par, fn = obj2$fn, gr = obj2$gr, method = "BFGS", control=list(trace=TRUE))
+#The answer always seems fine in the sense that it is what I expected it to
+#only shift is in rho but I am unsure if it has it's own story if its story is dependant on azzalini
+#The "story of the parameter" makes sense..As the d param grows, the less shared users matters
+#This is consistent with what we have been doing. 
+#I am worried I am forcing it too much and overfitting? Is that possible
 
+#Now with the expand grid I had some troubles.
+#Facetted along log sd county and log sd year because those were the 2 params I didnt care about
+#but I would have rather this been rho and offset
+#I think I want to do tile with d and theta and fill likelihood
+#Just confused about what to do here in general
+modellikelihood <- read.csv("data/TMBnllprofile.csv")
 
-#Looking at Azzalini with optim params
-distmat <- read.csv("data/distancematrix.csv")
-distmat2 <- distmat[,-1]
-rownames(distmat2) <- colnames(distmat2)
-d1 <- as.matrix(distmat2)
-diag(d1) <- 0
+likesurface <- ggplot(data = modellikelihood) +
+  geom_tile(aes(x = d, y = theta), fill = gridlikelihood) + #yikes im so confused as to what I am geom tiling
+  facet_grid(logsdcounty ~ logsdyears) 
+likesurface  
 
-
-cutoffpoint <- exp(-(900/exp(-10.75))^0.26)
-azzelinifun <- exp(-((d1)/exp(-10.75))^0.262)
-diag(azzelinifun) <- 0
-azzelinifun[(azzelinifun < cutoffpoint)] <- 0
-
-#Now to weight matrix
-localcountymat.w <- mat2listw(azzelinifun, style = "W")
-localcountymat.m <- as(localcountymat.w, "CsparseMatrix") 
-localcountymat <- as.matrix(localcountymat.m)
+#Did not get to simulations
 
